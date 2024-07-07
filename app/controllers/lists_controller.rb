7bee3e0a92e7771264be65cf1f6524094e14b886
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-07-07 18:10:56 +0900

    completed the list controller action methods and views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,30 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+
+    if @list.save
+      redirect_to @list, notice: "#{@list.name} was successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.required(:list).permit(:name)
+  end
 end
