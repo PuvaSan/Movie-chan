d0aafc7e084c9198330cde23bd7f2f704f3636c2
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-07-08 12:46:40 +0900

    finished the destroy action and app mvp completed

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -18,6 +18,9 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
