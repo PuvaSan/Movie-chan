c0cc436c223c57552ae99179f66b07bbfe9cd5df
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-07-09 19:53:30 +0900

    setup root path and bootstrap of lists page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
+  root "lists#index"
   resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,39 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+require "json"
+require "open-uri"
+
+puts "destroying all current movies in db..."
+Movie.destroy_all
+
+puts "connecting to movies api"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+puts "connected! downloading movies data"
+
+movies['results'].each do |movie|
+  puts movie["original_title"]
+
+  Movie.create!(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original" + movie["poster_path"], rating: ["vote_average"])
+end
+
+puts "finished top rated movies"
+puts "starting popular movies"
+
+url = "https://tmdb.lewagon.com/movie/popular"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+puts "connected! downloading movies data"
+
+movies['results'].each do |movie|
+  puts movie["original_title"]
+
+  Movie.create!(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original" + movie["poster_path"], rating: ["vote_average"])
+end
+
+puts "finished all movies"
