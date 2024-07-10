73e327bf5b5319e5b3b00da7d290bc894799d3cc
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-07-10 13:27:11 +0900

    add more css and bootstrap to index and show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,31 +16,67 @@ Movie.destroy_all
 
 puts "connecting to movies api"
 
-url = "https://tmdb.lewagon.com/movie/top_rated"
-movies_serialized = URI.open(url).read
-movies = JSON.parse(movies_serialized)
+# url = "https://tmdb.lewagon.com/movie/top_rated"
+# movies_serialized = URI.open(url).read
+# movies = JSON.parse(movies_serialized)
 
-puts "connected! downloading movies data"
+# puts "connected! downloading movies data"
 
-movies['results'].each do |movie|
-  puts movie["original_title"]
+# movies['results'].each do |movie|
+#   puts movie["original_title"]
 
-  Movie.create!(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original" + movie["poster_path"], rating: ["vote_average"])
-end
+#   Movie.create!(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original" + movie["poster_path"], rating: movie["vote_average"])
+# end
 
-puts "finished top rated movies"
-puts "starting popular movies"
+# puts "finished top rated movies"
+# puts "starting popular movies"
 
-url = "https://tmdb.lewagon.com/movie/popular"
-movies_serialized = URI.open(url).read
-movies = JSON.parse(movies_serialized)
+# url = "https://tmdb.lewagon.com/movie/popular"
+# movies_serialized = URI.open(url).read
+# movies = JSON.parse(movies_serialized)
 
-puts "connected! downloading movies data"
+# puts "connected! downloading movies data"
 
-movies['results'].each do |movie|
-  puts movie["original_title"]
+# movies['results'].each do |movie|
+#   puts movie["original_title"]
 
-  Movie.create!(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original" + movie["poster_path"], rating: ["vote_average"])
+#   Movie.create!(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original" + movie["poster_path"], rating: movie["vote_average"])
+# end
+
+# puts "finished all movies"
+
+
+
+
+movie_urls = ["https://tmdb.lewagon.com/movie/popular",
+              "https://tmdb.lewagon.com/movie/top_rated"]
+
+movie_urls.each do |url|
+  movies_serialized = URI.open(url).read
+  movies = JSON.parse(movies_serialized)
+  puts "starting #{url}"
+
+  movies['results'].each do |movie|
+    puts movie["original_title"]
+    Movie.create!(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original" + movie["poster_path"], rating: movie["vote_average"])
+  end
 end
 
 puts "finished all movies"
+
+tv_urls = ["https://tmdb.lewagon.com/tv/top_rated",
+          "https://tmdb.lewagon.com/tv/popular"]
+
+tv_urls.each do |url|
+  shows_serialized = URI.open(url).read
+  shows = JSON.parse(shows_serialized)
+  puts "starting #{url}"
+
+  shows['results'].each do |show|
+    puts show["original_name"]
+    next if show["overview"] == ""
+    Movie.create!(title: show["original_name"], overview: show["overview"] || "no overview", poster_url: "https://image.tmdb.org/t/p/original" + show["backdrop_path"], rating: show["vote_average"])
+  end
+end
+
+puts "finished all shows"
